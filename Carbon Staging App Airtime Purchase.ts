14581e5e5cd7276e923268a78588e97ef446ebae
AlabiMichael <62624167+Overflowmike@users.noreply.github.com>
2022-01-10 11:38:05 +0100

    Test Report

---
+++ Carbon Staging App Airtime Purchase.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Carbon Staging App Airtime Purchase</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>260d3079-9016-4e37-92c4-88add9566f8c</testSuiteGuid>
+   <testCaseLink>
+      <guid>599a14d6-cf40-4227-98e2-85de5667dd5e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Airtime Purchase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Carbon Staging App Fund Wallet.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Carbon Staging App Fund Wallet</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f1d9d111-19e0-4898-a984-c010b1434802</testSuiteGuid>
+   <testCaseLink>
+      <guid>e4b5783b-cd06-434f-ab9a-40c9c8121fae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fund Wallet</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
