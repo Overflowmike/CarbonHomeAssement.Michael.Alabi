bba5798ab8204677ff68627ce5045da56898d5c9
AlabiMichael <62624167+Overflowmike@users.noreply.github.com>
2022-01-10 11:45:35 +0100

    Test Suites

---
+++ Test Suites/Carbon Staging App Sign In.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Verify that a user can sign in successfully to Carbon Staging App</description>
+   <name>Carbon Staging App Sign In</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7565f716-8c23-44d1-9882-f793718c056e</testSuiteGuid>
+   <testCaseLink>
+      <guid>b0a23005-6844-4908-a5e8-13b783d43565</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sign In Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>18b353aa-a2f4-4d45-a099-c09a7a7fa443</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fund Wallet</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Carbon Staging App Wallet Transaction Filter.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Carbon Staging App Wallet Transaction Filter</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>68918d3a-e67d-461e-8839-70d12be1baff</testSuiteGuid>
+   <testCaseLink>
+      <guid>9e17e44c-6080-49ce-876f-8254f45a2225</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Wallet Transaction Filter</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
